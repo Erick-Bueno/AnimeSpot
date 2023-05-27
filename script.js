c4b867117582e41f5cfc5fd1274d50a7e6bf4aa0
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-05-27 01:39:40 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let paragrafo = document.querySelector(".cop")
+let paragrafo2 = document.querySelector(".cop2")
+
+let date = new Date()
+let yearNow = date.getFullYear()
+
+
+
+paragrafo.textContent = yearNow
+paragrafo2.textContent = yearNow

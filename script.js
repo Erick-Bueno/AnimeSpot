c043bf9405dfd00884fef2fa35231dbcaf77770c
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-05-27 23:23:56 -0300

    page manga implemented

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ let paragrafo2 = document.querySelector(".cop2")
 let date = new Date()
 let yearNow = date.getFullYear()
 
+let current_page = window.location.href
+let links = document.querySelectorAll("a")
 
+links.forEach(function(link){
+    console.log(link.getAttribute("href"))
+    console.log(current_page)
+    if(link.href === current_page){
+        link.classList.add("current-page")
+        
+    }
+})
 
 paragrafo.textContent = yearNow
 paragrafo2.textContent = yearNow

1f9d04cd174490a0e63394568901db05641637d1
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-06-03 17:30:44 -0300

    Update inserirdados.php

--- pages/Admin/dashboard/inserirdados.php
+++ pages/Admin/dashboard/inserirdados.php
@@ -7,8 +7,8 @@
     $tipo_conteudo = $_POST['typeContent'];
     $tipo_noticia = $_POST['typeNotice'];
     $conteudo = $_POST['conteudo'];
-    $acesso_img = "https://animespot-production.up.railway.app/imagemNoticias/" . $nome_arquivo;
-    $diretorio = "https://animespot-production.up.railway.app/imagemNoticias/";
+    $acesso_img = $_SERVER['DOCUMENT_ROOT'] . "//imagemNoticias//" . $nome_arquivo;
+    $diretorio = $_SERVER['DOCUMENT_ROOT'] . "/imagemNoticias";
     $caminho = $diretorio . $nome_arquivo;
     $query = "";
     if(move_uploaded_file($arquivo_tmp,$caminho)){

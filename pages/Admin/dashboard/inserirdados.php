b2fd5f3744dde2e391ad916d0bab13fb385e6bdc
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-06-03 17:10:03 -0300

    sasd

--- pages/Admin/dashboard/inserirdados.php
+++ pages/Admin/dashboard/inserirdados.php
@@ -7,8 +7,8 @@
     $tipo_conteudo = $_POST['typeContent'];
     $tipo_noticia = $_POST['typeNotice'];
     $conteudo = $_POST['conteudo'];
-    $acesso_img = "animespot-production.up.railway.app/AnimeSpot/imagemNoticias/" . $nome_arquivo;
-    $diretorio = "C:/xampp/htdocs/AnimeSpot/imagemNoticias/";
+    $acesso_img = "https://animespot-production.up.railway.app/imagemNoticias/" . $nome_arquivo;
+    $diretorio = "https://animespot-production.up.railway.app/imagemNoticias";
     $caminho = $diretorio . $nome_arquivo;
     $query = "";
     if(move_uploaded_file($arquivo_tmp,$caminho)){

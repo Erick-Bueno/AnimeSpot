7555ddf1c848b8073a9942bd02d55db18a72c138
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-06-04 02:39:24 -0300

    Update atualizarNoticia.js

--- pages/Admin/dashboard/atualizarNoticia.js
+++ pages/Admin/dashboard/atualizarNoticia.js
@@ -13,7 +13,13 @@ let form = document.querySelector("form")
 
 
 let id_notice = parametros.get("id")
-
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0,
+        v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
 async function buscarNoticia(){
     let req = await fetch(`../../buscarNoticia.php?id=${id_notice}`)
@@ -47,29 +53,78 @@ inputFile.addEventListener("change", function(e){
 })
 form.addEventListener("submit", async function(e){
     e.preventDefault()
-    let formdata = new FormData(this)
-    let req = await fetch(`update.php?id=${id_notice}`,{
-        method:"POST",
-        body:formdata
-    })
-    let res = await req.json()
+    if(inputFile.files.length > 0){
+        let uuid = generateUUID()
+        let img_selected = inputFile.files[0]
+        let nomeimg = uuid + "_" + img_selected.name
+        const firebaseConfig = {
+          apiKey: "AIzaSyDVWdg2eS3We3myqVLfFYV6xN4UAXHrSho",
+          authDomain: "lateral-rider-354218.firebaseapp.com",
+          projectId: "lateral-rider-354218",
+          storageBucket: "lateral-rider-354218.appspot.com",
+          messagingSenderId: "146567212142",
+          appId: "1:146567212142:web:7ecfb9119b3283e87dca7f",
+          measurementId: "G-ZJDLVTSDX0"
+        };
+        if (!firebase.apps.length) {
+            // Configurar o Firebase usando o seu Firebase config
+            firebase.initializeApp(firebaseConfig);
+          }
+          let storage = firebase.storage()
+          storage.ref().child("images").child(nomeimg).put(img_selected).then(function(snapshot){
+            snapshot.ref.getDownloadURL().then( async function(url){
+                let dowUrl = encodeURIComponent(url)
+                let formdata = new FormData(form);
+                let req = await fetch(`update.php?id=${id_notice}?imgurl=${imgurl}`,{
+                    method:"POST",
+                    body:formdata
+                })
+                let res = await req.json()
+                let div_msg = document.createElement("div")
+                div_msg.classList.add("msg_sucesss")
+                let p_msg_content = document.createElement("p")
+                p_msg_content.classList.add("msg_noticia")
+                div_msg.appendChild(p_msg_content)
+                
+                document.body.appendChild(div_msg)
+                div_msg.style.display = 'block'
+                p_msg_content.textContent = res.msg
+                setTimeout(() => {
+                    div_msg.classList.add("fade-out")
+                }, 1500);
+                setTimeout(() => {
+                    div_msg.classList.remove("fade-out")
+                    div_msg.style.display = 'none'
+                    div_msg.classList.remove("animating");
+                }, 2500);
+            })
+
+          })
+    }
+
+  let req = await fetch(`update.php?id=${id_notice}`,{
+                    method:"POST",
+                    body:formdata
+                })
+                let res = await req.json()
+                let div_msg = document.createElement("div")
+                div_msg.classList.add("msg_sucesss")
+                let p_msg_content = document.createElement("p")
+                p_msg_content.classList.add("msg_noticia")
+                div_msg.appendChild(p_msg_content)
+                
+                document.body.appendChild(div_msg)
+                div_msg.style.display = 'block'
+                p_msg_content.textContent = res.msg
+                setTimeout(() => {
+                    div_msg.classList.add("fade-out")
+                }, 1500);
+                setTimeout(() => {
+                    div_msg.classList.remove("fade-out")
+                    div_msg.style.display = 'none'
+                    div_msg.classList.remove("animating");
+                }, 2500);
    
-    let div_msg = document.createElement("div")
-    div_msg.classList.add("msg_sucesss")
-    let p_msg_content = document.createElement("p")
-    p_msg_content.classList.add("msg_noticia")
-    div_msg.appendChild(p_msg_content)
-    
-    document.body.appendChild(div_msg)
-    div_msg.style.display = 'block'
-    p_msg_content.textContent = res.msg
-    setTimeout(() => {
-        div_msg.classList.add("fade-out")
-    }, 1500);
-    setTimeout(() => {
-        div_msg.classList.remove("fade-out")
-        div_msg.style.display = 'none'
-        div_msg.classList.remove("animating");
-    }, 2500);
+   
 })
 buscarNoticia()

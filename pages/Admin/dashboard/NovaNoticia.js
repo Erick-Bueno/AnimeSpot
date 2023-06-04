13f90c0a336e796b3ac795373fbdca92d0f3dacb
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2023-06-04 01:36:13 -0300

    Update NovaNoticia.js

--- pages/Admin/dashboard/NovaNoticia.js
+++ pages/Admin/dashboard/NovaNoticia.js
@@ -58,8 +58,39 @@ inputDescription.addEventListener("input", function (e) {
   }
   let storage = firebase.storage()
   storage.ref().child("images").child(nomeimg).put(img_selected).then(function(snapshot){
-        snapshot.ref.getDownloadURL().then(function(url){
+        snapshot.ref.getDownloadURL().then( async function(url){
             console.log(url)
+            let formdata = new FormData(this);
+            let req = await fetch(`inserirdados.php?urlimg=${url}`, {
+              method: "POST",
+              body: formdata,
+            });
+          
+            console.log(formdata);
+          
+            let res = await req.json();
+            form.reset();
+            a.src = "";
+            paragraphDescription.textContent = "Descrição";
+            paragraphTitle.textContent = "Titulo";
+          
+            let div_msg = document.createElement("div");
+            div_msg.classList.add("msg_sucesss");
+            let p_msg_content = document.createElement("p");
+            p_msg_content.classList.add("msg_noticia");
+            div_msg.appendChild(p_msg_content);
+          
+            document.body.appendChild(div_msg);
+            div_msg.style.display = "block";
+            p_msg_content.textContent = res.message;
+            setTimeout(() => {
+              div_msg.classList.add("fade-out");
+            }, 1500);
+            setTimeout(() => {
+              div_msg.classList.remove("fade-out");
+              div_msg.style.display = "none";
+              div_msg.classList.remove("animating");
+            }, 2500);  
         })
  
   })
@@ -68,35 +99,5 @@ inputDescription.addEventListener("input", function (e) {
 }
 
 
-  /*  let formdata = new FormData(this);
-  let req = await fetch(`inserirdados.php?urlimg=${}`, {
-    method: "POST",
-    body: formdata,
-  });
-
-  console.log(formdata);
-
-  let res = await req.json();
-  form.reset();
-  a.src = "";
-  paragraphDescription.textContent = "Descrição";
-  paragraphTitle.textContent = "Titulo";
-
-  let div_msg = document.createElement("div");
-  div_msg.classList.add("msg_sucesss");
-  let p_msg_content = document.createElement("p");
-  p_msg_content.classList.add("msg_noticia");
-  div_msg.appendChild(p_msg_content);
-
-  document.body.appendChild(div_msg);
-  div_msg.style.display = "block";
-  p_msg_content.textContent = res.message;
-  setTimeout(() => {
-    div_msg.classList.add("fade-out");
-  }, 1500);
-  setTimeout(() => {
-    div_msg.classList.remove("fade-out");
-    div_msg.style.display = "none";
-    div_msg.classList.remove("animating");
-  }, 2500);  */
+ 
 }); 
